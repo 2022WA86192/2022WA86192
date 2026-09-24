70abb103b7c75df2879d73379bf0e741af8ff45c
Deepu R Nandihalli <2022WA86192@wilp.bits-pilani.ac.in>
2026-09-24 22:23:44 +0530

    Modified source code for continuous integration

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -2,6 +2,6 @@ package com.bits.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("BITS DevOps Jenkins CI Build Successful!");
+        System.out.println("BITS DevOps CI Build #4 Successful!");
     }
 }
